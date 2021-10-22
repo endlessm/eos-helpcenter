57d19774ac258eea4e090a67b5cb3f6bbc668ef5
Dan Nicholson <dbn@endlessos.org>
2021-10-22 13:08:12 -0600

    generate-html-docs: Remove custom XSL and CSS handling
    We've dropped all the Endless customizations for the generated docs, so
    now the script simply needs to call `yelp-build`. All the assets will be
    included in the build directory.
    
    https://phabricator.endlessm.com/T30675

--- generate-html-docs.sh
+++ generate-html-docs.sh
@@ -2,7 +2,6 @@
 
 SRCDIR=$(dirname "$0")
 DEST="$SRCDIR/build"
-XSL="$SRCDIR/endless-customizations.xsl"
 
 function show_need_yelp() {
     echo "  ERROR: this script requires 'yelp-build'"
@@ -17,23 +16,19 @@ function show_need_yelp() {
 
 function usage() {
     cat <<EOF
-Usage: $0 [OPTION]... SRC XSL DEST
+Usage: $0 [OPTION]... SRC DEST
 
   -h, --help        display this help and exit
 EOF
 }
 
-ARGS=$(getopt -o o:x:h -l outdir:,xsl:,help -n "$0" -- "$@")
+ARGS=$(getopt -o o:h -l outdir:,help -n "$0" -- "$@")
 eval set -- "$ARGS"
 
 while true; do
     case "$1" in
 	-o|--outdir)
 	    DEST=$2
-	    shift 2
-	    ;;
-	-x|--xsl)
-	    XSL=$2
 	    shift 2
 	    ;;
         -h|--help)
@@ -53,9 +48,6 @@ echo ""
 echo "  Endless OS Doc2HTML script"
 echo ""
 
-# XSLT path for finding Endless yelp-xsl components
-xslt_path=/usr/share/yelp-xsl/xslt/common/domains
-
 # Delete existing build directory
 if [ -d "$DEST" ]; then
     echo "  Deleting existing documentation in $DEST"
@@ -73,19 +65,10 @@ for doc_path in /usr/share/help/*/gnome-help; do
 
     echo -n "      $lang..."
 
-    yelp-build html -o "$doc_out" -x "$XSL" -p "$xslt_path" \
-        "$doc_path"/*.page
+    yelp-build html -o "$doc_out" "$doc_path"/*.page
 
     echo " OK"
 
-done
-
-# Copy the Endless CSS and images to root
-echo "  Including assets in $DEST..."
-for asset in css img; do
-    echo -n "      $asset..."
-    cp -r /usr/share/yelp/endless/"$asset" "$DEST"
-    echo " OK"
 done
 
 echo ""
