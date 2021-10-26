21ba87c49aa86533c82a39aae55fd696f9125c34
Dan Nicholson <dbn@endlessos.org>
2021-10-26 12:21:35 -0600

    publish-docs: Use boto3 for better upload control
    `aws s3 sync` pretty much does what we want, but there's no reliable way
    to know what objects it updated without scraping the output. Knowing
    exactly which objects changed will be useful in order to invalidate them
    in the CloudFront CDN. Otherwise we have to do a full distribution
    invalidation every time.
    
    We can also do a little better than just looking at file sizes for
    non-multipart uploads by calculating an expected ETag and comparing it.
    It would be nice if S3 would just give you the checksum, but alas.
    
    https://phabricator.endlessm.com/T31257

--- publish-docs.py
+++ publish-docs.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+import boto3
+import hashlib
+import logging
 import os
-import subprocess
+
+logger = logging.getLogger(os.path.basename(__file__))
 
 SRCDIR = os.path.dirname(__file__)
 BUILDDIR = os.path.join(SRCDIR, 'build')
@@ -14,20 +18,86 @@ ap.add_argument('-d', '--builddir', default=BUILDDIR,
 ap.add_argument('--region', help='AWS region of the S3 bucket')
 ap.add_argument('-n', '--dry-run', action='store_true',
                 help='only show what would be published')
+ap.add_argument('--debug', action='store_true',
+                help='enable debugging messages')
+ap.add_argument('--aws-debug', action='store_true',
+                help='enable AWS debugging messages')
 args = ap.parse_args()
 
-cmd = ['aws', 's3', 'sync']
-if args.dry_run:
-    cmd.append('--dryrun')
-if args.region:
-    cmd += ['--region', args.region]
-cmd += [
-    '--delete',
-    # Build timestamps are always updated during the build, so ignore them
-    '--size-only',
-    args.builddir,
-    's3:///{}'.format(args.bucket)
-]
+logging.basicConfig(level=logging.INFO)
+if args.debug:
+    logger.setLevel(logging.DEBUG)
+if args.aws_debug:
+    boto3_logger = logging.getLogger('boto3')
+    boto3_logger.setLevel(logging.DEBUG)
 
-print('Syncing', args.builddir, 'to', args.bucket)
-subprocess.check_call(cmd)
+s3 = boto3.resource('s3', region_name=args.region)
+bucket = s3.Bucket(args.bucket)
+
+# Collect all the generated docs into a dictionary.
+logger.info('Finding docs files in directory %s', args.builddir)
+docs = {}
+for root, dirs, files in os.walk(args.builddir):
+    for name in files:
+        path = os.path.join(root, name)
+        key = os.path.relpath(path, args.builddir)
+        with open(path, 'rb') as f:
+            md5sum = hashlib.md5(f.read()).hexdigest()
+        docs[key] = {
+            'path': path,
+            'size': os.path.getsize(path),
+            'e_tag': f'"{md5sum}"',
+        }
+
+# Collect all the current objects and their ETags in the bucket into a
+# dictionary.
+logger.info('Finding current objects in bucket %s', args.bucket)
+objects = dict([
+    (obj.key, obj) for obj in bucket.objects.all()
+])
+
+# Keep a list of changed objects.
+changed = []
+
+# Upload all the new objects.
+for key, doc in sorted(docs.items()):
+    logger.debug(f'Considering {key} for upload')
+
+    # See if there are any changes from an existing object.
+    obj = objects.get(key)
+    if obj:
+        logger.debug(
+            f'Comparing {key} to existing S3 object, '
+            f'local: size={doc["size"]}, e_tag={doc["e_tag"]}, '
+            f's3: size={obj.size}, e_tag={obj.e_tag}'
+        )
+
+        # For a non-multipart-upload, S3 will use a quoted md5sum for
+        # the ETag and we can compare them directly.
+        if len(doc['e_tag']) == len(obj.e_tag):
+            if doc['e_tag'] == obj.e_tag:
+                logger.debug(f'Skipping {key} upload, matching ETag')
+                continue
+        else:
+            # Otherwise the ETag is generated in S3 and the best we can
+            # do is a size comparison since the local timestamps are
+            # meaningless.
+            if doc['size'] == obj.size:
+                logger.debug(f'Skipping {key} upload, matching size')
+                continue
+
+    logger.info(f'Uploading {key}')
+    changed.append(key)
+    if not args.dry_run:
+        bucket.upload_file(doc['path'], key)
+
+# Delete any objects that are in S3 but not in the generated docs.
+for key in objects.keys() - docs.keys():
+    logger.info(f'Deleting {key}')
+    obj = objects[key]
+    changed.append(key)
+    if not args.dry_run:
+        obj.delete()
+
+if len(changed) == 0:
+    logger.info('All objects up to date')
